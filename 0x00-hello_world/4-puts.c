9ff066f5a8890304d8c02dcc6c10cc3772ba2e1d
Matth-py <matthewoluwatobiojo@gmail.com>
2022-09-10 18:25:49 +0100

    c hello world new file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,10 @@
  * main - entry point
  *
  * Return:always return 0
+ *
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
