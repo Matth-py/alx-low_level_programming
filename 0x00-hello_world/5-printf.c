8cec0e6a76b235472e115505099b5fc44fcd4ee4
Matth-py <matthewoluwatobiojo@gmail.com>
2022-09-11 22:27:14 +0100

    c hello world new file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return: Always return 0
- *
+ * Return: always 0
  */
-int main(void) 
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
